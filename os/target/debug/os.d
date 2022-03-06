f8b8b3d81de9d258f2be3298415ea4d3adf26cfe
siwen <siwensenta@163.com>
2022-03-06 23:44:56 +0800

    hello

---
+++ os/target/debug/os.d
@@ -0,0 +1,1 @@
+/home/siwen/learn_os/os/target/debug/os: /home/siwen/learn_os/os/src/main.rs
---
+++ os/target/riscv64gc-unknown-none-elf/debug/deps/os-1ee2bc86448aa2d0.d
@@ -0,0 +1,5 @@
+/home/siwen/learn_os/os/target/riscv64gc-unknown-none-elf/debug/deps/os-1ee2bc86448aa2d0: src/main.rs
+
+/home/siwen/learn_os/os/target/riscv64gc-unknown-none-elf/debug/deps/os-1ee2bc86448aa2d0.d: src/main.rs
+
+src/main.rs:
